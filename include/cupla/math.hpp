1e47478de6791be9cb1a63ecbd68b5d3e80a8f05
Third Party <picongpu@hzdr.de>
2020-04-06 11:20:33 +0200

    Squashed 'thirdParty/cupla/' changes from ed00837ef6..d6310a037e
    d6310a037e Merge pull request #165 from psychocoderHPC/topic-updateGithubLinks2
    e7f9261bf7 update cupla github links
    f4dba001d4 Merge pull request #162 from psychocoderHPC/topic-cuplaAccMathFunctions
    bbbc0b0266 `BlackScholes` use cupla math functions
    efa8d61037 on device math functions
    dbc0f649b2 Merge pull request #160 from psychocoderHPC/topic-updateAlpakaTo0.5.0dev
    b31fc4ea33 Merge commit '13b737ba6cd38400636d6a6a52dee81b21a7412c' into topic-updateAlpakaTo0.5.0dev
    13b737ba6c Squashed 'alpaka/' changes from ab0b8a460..c2d14d09e
    09d5daf1ff update travis tests
    582f8ca10e update CMake requirements
    59fba492fa include changes needed to use alpaka 0.5.0
    
    git-subtree-dir: thirdParty/cupla
    git-subtree-split: d6310a037e7207325e951a2ef5712268804a5b91

---
+++ include/cupla/math.hpp
@@ -0,0 +1,31 @@
+/* Copyright 2020 Rene Widera
+ *
+ * This file is part of cupla.
+ *
+ * cupla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cupla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with cupla.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#pragma once
+
+#include "cupla/math/Abs.hpp"
+#include "cupla/math/Comparison.hpp"
+#include "cupla/math/Exp.hpp"
+#include "cupla/math/Log.hpp"
+#include "cupla/math/Mod.hpp"
+#include "cupla/math/Root.hpp"
+#include "cupla/math/Round.hpp"
+#include "cupla/math/Trigo.hpp"
