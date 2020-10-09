804d0f741d0d3672d4e1c8129390c22037567b54
René Widera <r.widera@hzdr.de>
2020-10-09 08:18:35 +0200

    working version with KHI

--- include/picongpu/param/variables.hpp
+++ include/picongpu/param/variables.hpp
@@ -3,17 +3,17 @@
 namespace picongpu
 {
 
-    DEF_PARAMETER(float_64, DELTA_T, "delta_t", "simulation time step");
+    DEF_PARAMETER(float_64, DELTA_T, "sim.delta_t", "simulation time step");
     PARAM_ADD(float_X, DELTA_T, PIC);
 
 
-    DEF_PARAMETER(float_64, CELL_WIDTH, "delta_x", "cell size in x");
+    DEF_PARAMETER(float_64, CELL_WIDTH, "sim.cellSize_x", "cell size in x");
     PARAM_ADD(float_X, CELL_WIDTH, PIC);
 
-    DEF_PARAMETER(float_64, CELL_HEIGHT, "delta_y", "cell size in y");
+    DEF_PARAMETER(float_64, CELL_HEIGHT, "sim.cellSize_y", "cell size in y");
     PARAM_ADD(float_X, CELL_HEIGHT, PIC);
 
-    DEF_PARAMETER(float_64, CELL_DEPTH, "delta_z", "cell size in z");
+    DEF_PARAMETER(float_64, CELL_DEPTH, "sim.cellSize_z", "cell size in z");
     PARAM_ADD(float_X, CELL_DEPTH, PIC);
 
 } // namespace picongpu
