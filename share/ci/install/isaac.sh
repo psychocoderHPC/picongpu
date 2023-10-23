87da1b12a3820ed5489eb51bb7d52005a290895a
René Widera <r.widera@hzdr.de>
2023-10-23 15:27:32 +0200

    CI: enable ISAACfor HIP
    fix #4714
    
    - switch to latest ISAAC dev https://github.com/ComputationalRadiationPhysics/isaac/tree/014dc16e9ab192821ba941e1ceb0c313d94b71fe
    - switch to latest glm master https://github.com/g-truc/glm/tree/47585fde0c49fa77a2bf2fb1d2ead06999fd4b6e

--- share/ci/install/isaac.sh
+++ share/ci/install/isaac.sh
@@ -11,7 +11,7 @@ export GLM_ROOT=/opt/glm/0.9.9.9-dev
 export CMAKE_PREFIX_PATH=$GLM_ROOT:$CMAKE_PREFIX_PATH
 git clone https://github.com/g-truc/glm.git
 cd glm
-git checkout 6ad79aae3eb5bf809c30bf1168171e9e55857e45
+git checkout 47585fde0c49fa77a2bf2fb1d2ead06999fd4b6e
 mkdir build
 cd build
 cmake ../ -DCMAKE_INSTALL_PREFIX=$GLM_ROOT -DGLM_TEST_ENABLE=OFF
@@ -20,7 +20,7 @@ make install
 cd $CI_PROJECT_DIR
 git clone https://github.com/ComputationalRadiationPhysics/isaac.git
 cd isaac
-git checkout 195420ca1c43f6148c7f6c3a10ad9cad32e04d6a
+git checkout 014dc16e9ab192821ba941e1ceb0c313d94b71fe
 mkdir build_isaac
 cd build_isaac
 cmake ../lib/ -DCMAKE_INSTALL_PREFIX=$ISAAC_ROOT
